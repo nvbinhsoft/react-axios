2f29d81264182708640396abea9f305c081393d8
nvbinhsoft <nvbinhsoft@gmail.com>
2024-09-24 12:51:49 +0700

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Posts from "./components/Posts.tsx";
+
+function App() {
+
+  return (
+      <div className={"App"}>
+          <Posts></Posts>
+      </div>
+  )
+}
+
+export default App
