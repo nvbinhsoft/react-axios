c70ae1db59918c12762514b402f31239b3bf5b6e
nvbinhsoft <nvbinhsoft@gmail.com>
2024-09-24 12:52:01 +0700

    feat: create post service

---
+++ src/services/postService.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+const api = axios.create({
+    baseURL: "https://jsonplaceholder.typicode.com"
+    });
+
+export interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+ const getPosts = (): Promise<{data: Post[]}> => {
+    return api.get("/posts");
+};
+
+const deletePost = (id: number) : Promise<void> => api.delete(`/posts/${id}`);
+
+export { deletePost, getPosts };
